1b0860e4dbae46a97d91859f7184229ffe3c9c5b
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-12 14:29:08 +0530

    practice: update aws networking outputs for 3-tier subnets, nat gateways, and security groups

--- aws/networking/outputs.tf
+++ aws/networking/outputs.tf
@@ -4,16 +4,36 @@ output "vpc_id" {
 }
 
 output "public_subnet_ids" {
-  description = "IDs of the public subnets"
-  value       = aws_subnet.public[*].id
+  description = "IDs of the public subnets, keyed by AZ suffix"
+  value       = { for k, s in aws_subnet.public : k => s.id }
 }
 
 output "private_subnet_ids" {
-  description = "IDs of the private subnets"
-  value       = aws_subnet.private[*].id
+  description = "IDs of the private subnets, keyed by AZ suffix"
+  value       = { for k, s in aws_subnet.private : k => s.id }
 }
 
-output "internet_gateway_id" {
-  description = "ID of the Internet Gateway"
-  value       = aws_internet_gateway.main.id
+output "database_subnet_ids" {
+  description = "IDs of the database subnets, keyed by AZ suffix"
+  value       = { for k, s in aws_subnet.database : k => s.id }
+}
+
+output "nat_gateway_ids" {
+  description = "IDs of the NAT Gateways, keyed by AZ suffix"
+  value       = { for k, n in aws_nat_gateway.main : k => n.id }
+}
+
+output "web_sg_id" {
+  description = "ID of the web tier security group"
+  value       = aws_security_group.web.id
+}
+
+output "app_sg_id" {
+  description = "ID of the app tier security group"
+  value       = aws_security_group.app.id
+}
+
+output "database_sg_id" {
+  description = "ID of the database tier security group"
+  value       = aws_security_group.database.id
 }
