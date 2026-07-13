1d3fdf90d6b4a6d803ce85bfd2bd05d83e3e7941
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-13 18:29:10 +0530

    practice: update azure networking outputs for nat gateway and asg ids

--- azure/networking/outputs.tf
+++ azure/networking/outputs.tf
@@ -8,12 +8,37 @@ output "vnet_id" {
   value       = azurerm_virtual_network.main.id
 }
 
-output "subnet_ids" {
-  description = "IDs of the subnets, keyed by subnet name"
-  value       = { for k, s in azurerm_subnet.main : k => s.id }
+output "public_subnet_ids" {
+  description = "IDs of the public subnets, keyed by name suffix"
+  value       = { for k, s in azurerm_subnet.public : k => s.id }
 }
 
-output "nsg_ids" {
-  description = "IDs of the network security groups, keyed by subnet name"
-  value       = { for k, n in azurerm_network_security_group.main : k => n.id }
+output "private_subnet_ids" {
+  description = "IDs of the private subnets, keyed by name suffix"
+  value       = { for k, s in azurerm_subnet.private : k => s.id }
+}
+
+output "database_subnet_ids" {
+  description = "IDs of the database subnets, keyed by name suffix"
+  value       = { for k, s in azurerm_subnet.database : k => s.id }
+}
+
+output "nat_gateway_id" {
+  description = "ID of the NAT Gateway"
+  value       = azurerm_nat_gateway.main.id
+}
+
+output "web_asg_id" {
+  description = "ID of the web tier application security group"
+  value       = azurerm_application_security_group.web.id
+}
+
+output "app_asg_id" {
+  description = "ID of the app tier application security group"
+  value       = azurerm_application_security_group.app.id
+}
+
+output "database_asg_id" {
+  description = "ID of the database tier application security group"
+  value       = azurerm_application_security_group.database.id
 }
