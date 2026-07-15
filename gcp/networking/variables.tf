67c41ce87f251edbf43f01c287fd6c347105a638
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-15 18:48:15 +0530

    feat(gcp-networking): add input variables for vpc and subnet config

---
+++ gcp/networking/variables.tf
@@ -0,0 +1,34 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region to deploy resources in"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "environment" {
+  description = "Environment name (dev, staging, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.2.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private (app) subnet"
+  type        = string
+  default     = "10.2.11.0/24"
+}
+
+variable "database_subnet_cidr" {
+  description = "CIDR block for the database subnet"
+  type        = string
+  default     = "10.2.21.0/24"
+}
