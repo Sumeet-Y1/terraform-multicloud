be353170a3ac06cf2af25bb7799dd9190357880c
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-16 18:56:07 +0530

    feat(gcp-storage): add outputs for bucket name, url, and self link

---
+++ gcp/storage/outputs.tf
@@ -0,0 +1,14 @@
+output "bucket_name" {
+  description = "Name of the storage bucket"
+  value       = google_storage_bucket.main.name
+}
+
+output "bucket_url" {
+  description = "GCS URL of the bucket (gs://...)"
+  value       = google_storage_bucket.main.url
+}
+
+output "bucket_self_link" {
+  description = "Self link of the bucket"
+  value       = google_storage_bucket.main.self_link
+}
