d79db1ef272175355f4e85bf7333db72110f449a
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-21 20:29:44 +0530

    practice: add bastion firewall rule output for gcp networking

--- gcp/networking/outputs.tf
+++ gcp/networking/outputs.tf
@@ -4,7 +4,7 @@ output "vpc_id" {
 }
 
 output "vpc_self_link" {
-  description = "Self link of the VPC network (used when referencing this VPC from other resources)"
+  description = "Self link of the VPC network"
   value       = google_compute_network.main.self_link
 }
 
@@ -26,4 +26,9 @@ output "database_subnet_id" {
 output "router_id" {
   description = "ID of the Cloud Router"
   value       = google_compute_router.main.id
+}
+
+output "bastion_firewall_rule" {
+  description = "Name of the bastion SSH firewall rule (for reference/auditing)"
+  value       = google_compute_firewall.allow_bastion_ssh.name
 }
