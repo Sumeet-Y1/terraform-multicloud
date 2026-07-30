da6c67a2f1a9b2f9f30f923653ecf1c8ab8bc988
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-30 20:38:06 +0530

    practice: add outputs for s3 endpoint, bastion sg, and flow log group

--- aws/networking/outputs.tf
+++ aws/networking/outputs.tf
@@ -23,6 +23,16 @@ output "nat_gateway_ids" {
   value       = { for k, n in aws_nat_gateway.main : k => n.id }
 }
 
+output "s3_vpc_endpoint_id" {
+  description = "ID of the S3 VPC Gateway Endpoint"
+  value       = aws_vpc_endpoint.s3.id
+}
+
+output "bastion_sg_id" {
+  description = "ID of the bastion security group"
+  value       = aws_security_group.bastion.id
+}
+
 output "web_sg_id" {
   description = "ID of the web tier security group"
   value       = aws_security_group.web.id
@@ -36,4 +46,9 @@ output "app_sg_id" {
 output "database_sg_id" {
   description = "ID of the database tier security group"
   value       = aws_security_group.database.id
+}
+
+output "flow_log_group_name" {
+  description = "Name of the CloudWatch log group for VPC flow logs (null if disabled)"
+  value       = var.enable_flow_logs ? aws_cloudwatch_log_group.flow_logs[0].name : null
 }
