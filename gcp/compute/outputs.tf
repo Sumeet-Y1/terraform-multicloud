639e3c137a9d05a62ba9b106a149b955a6945426
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-02 14:18:33 +0530

    practice: update gcp compute outputs for mig, health check, and autoscaler ids

--- gcp/compute/outputs.tf
+++ gcp/compute/outputs.tf
@@ -1,14 +1,24 @@
-output "instance_ids" {
-  description = "IDs of the created instances"
-  value       = google_compute_instance.main[*].id
+output "instance_template_id" {
+  description = "ID of the instance template"
+  value       = google_compute_instance_template.main.id
 }
 
-output "instance_private_ips" {
-  description = "Private IP addresses of the created instances"
-  value       = google_compute_instance.main[*].network_interface[0].network_ip
+output "instance_group_manager_id" {
+  description = "ID of the managed instance group"
+  value       = google_compute_region_instance_group_manager.main.id
 }
 
-output "instance_names" {
-  description = "Names of the created instances"
-  value       = google_compute_instance.main[*].name
+output "instance_group_manager_instance_group" {
+  description = "URL of the underlying instance group (useful for attaching a load balancer)"
+  value       = google_compute_region_instance_group_manager.main.instance_group
+}
+
+output "health_check_id" {
+  description = "ID of the health check"
+  value       = google_compute_health_check.main.id
+}
+
+output "autoscaler_id" {
+  description = "ID of the autoscaler (null if autoscaling is disabled)"
+  value       = var.enable_autoscaling ? google_compute_region_autoscaler.main[0].id : null
 }
