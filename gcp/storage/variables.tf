3898ae8c9a91c649dd669edaa27151c341cb23f5
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-02 14:27:28 +0530

    practice: add multi-bucket, lifecycle, and iam variables for gcp storage

--- gcp/storage/variables.tf
+++ gcp/storage/variables.tf
@@ -15,19 +15,41 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "bucket_name" {
-  description = "Name of the storage bucket (must be globally unique)"
+variable "bucket_prefix" {
+  description = "Prefix for bucket names (final name = prefix-purpose, must be globally unique)"
   type        = string
 }
 
 variable "storage_class" {
-  description = "Storage class (STANDARD, NEARLINE, COLDLINE, ARCHIVE)"
+  description = "Default storage class (STANDARD, NEARLINE, COLDLINE, ARCHIVE)"
   type        = string
   default     = "STANDARD"
 }
 
-variable "enable_versioning" {
-  description = "Enable object versioning on the bucket"
-  type        = bool
-  default     = true
+variable "buckets" {
+  description = "Map of buckets to create: key = purpose suffix, value = enable versioning"
+  type        = map(bool)
+  default = {
+    "data"    = true
+    "logs"    = false
+    "backups" = true
+  }
+}
+
+variable "logs_archive_days" {
+  description = "Days before logs bucket objects move to Nearline storage class"
+  type        = number
+  default     = 30
+}
+
+variable "logs_delete_days" {
+  description = "Days before logs bucket objects are deleted entirely"
+  type        = number
+  default     = 90
+}
+
+variable "reader_members" {
+  description = "List of members (e.g. 'group:devs@company.com') granted read access to the data bucket"
+  type        = list(string)
+  default     = []
 }
