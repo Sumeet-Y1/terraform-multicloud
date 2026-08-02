dbcce73592dde6a073c340eb0119e01184197946
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-02 14:39:07 +0530

    practice: add asg, launch template, and scaling variables for aws compute

--- aws/compute/variables.tf
+++ aws/compute/variables.tf
@@ -22,13 +22,13 @@ variable "ami_id" {
   default     = ""
 }
 
-variable "subnet_id" {
-  description = "Subnet ID to launch the instance into (typically a private/app subnet)"
-  type        = string
+variable "subnet_ids" {
+  description = "List of subnet IDs for the Auto Scaling Group to launch instances into (typically private/app subnets across multiple AZs)"
+  type        = list(string)
 }
 
 variable "security_group_ids" {
-  description = "List of security group IDs to attach to the instance"
+  description = "List of security group IDs to attach to instances"
   type        = list(string)
 }
 
@@ -38,8 +38,26 @@ variable "key_name" {
   default     = ""
 }
 
-variable "instance_count" {
-  description = "Number of EC2 instances to create"
+variable "min_size" {
+  description = "Minimum number of instances in the Auto Scaling Group"
   type        = number
   default     = 1
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in the Auto Scaling Group"
+  type        = number
+  default     = 3
+}
+
+variable "desired_capacity" {
+  description = "Desired number of instances"
+  type        = number
+  default     = 2
+}
+
+variable "root_volume_size" {
+  description = "Size of the root EBS volume in GB"
+  type        = number
+  default     = 20
 }
