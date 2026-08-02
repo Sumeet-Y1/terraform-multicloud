cb4aef502398873c73e067ee8f297f93fb73d56a
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-02 14:18:27 +0530

    practice: add mig, autoscaling, and health check variables for gcp compute

--- gcp/compute/variables.tf
+++ gcp/compute/variables.tf
@@ -10,7 +10,7 @@ variable "region" {
 }
 
 variable "zone" {
-  description = "GCP zone to deploy the instance in"
+  description = "GCP zone for the health check region setting"
   type        = string
   default     = "us-central1-a"
 }
@@ -22,24 +22,48 @@ variable "environment" {
 }
 
 variable "machine_type" {
-  description = "GCP machine type (equivalent to AWS instance type / Azure VM size)"
+  description = "GCP machine type"
   type        = string
   default     = "e2-micro"
 }
 
 variable "subnet_id" {
-  description = "Self link or ID of the subnet to attach the instance to"
+  description = "Self link or ID of the subnet to attach instances to"
   type        = string
 }
 
 variable "network_tags" {
-  description = "Network tags for firewall targeting (e.g. web, app, database)"
+  description = "Network tags for firewall targeting"
   type        = list(string)
   default     = ["app"]
 }
 
-variable "instance_count" {
-  description = "Number of instances to create"
+variable "min_replicas" {
+  description = "Minimum number of instances in the managed instance group"
   type        = number
   default     = 1
+}
+
+variable "max_replicas" {
+  description = "Maximum number of instances in the managed instance group"
+  type        = number
+  default     = 3
+}
+
+variable "target_replicas" {
+  description = "Target/desired number of instances (used when autoscaling is disabled)"
+  type        = number
+  default     = 2
+}
+
+variable "enable_autoscaling" {
+  description = "Enable autoscaling based on CPU utilization"
+  type        = bool
+  default     = true
+}
+
+variable "cpu_target_utilization" {
+  description = "Target CPU utilization for autoscaling (0.0 to 1.0)"
+  type        = number
+  default     = 0.6
 }
