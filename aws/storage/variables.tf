02a54a374a258509081addd87e1f2150434545ca
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-04 21:20:39 +0530

    feat(aws-storage): add multi-bucket, lifecycle, and policy variables

--- aws/storage/variables.tf
+++ aws/storage/variables.tf
@@ -1,12 +1,7 @@
-variable "aws_region" {
+﻿variable "aws_region" {
   description = "AWS region to deploy resources in"
   type        = string
   default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket (must be globally unique)"
-  type        = string
 }
 
 variable "environment" {
@@ -15,8 +10,35 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "enable_versioning" {
-  description = "Enable versioning on the S3 bucket"
-  type        = bool
-  default     = true
+variable "bucket_prefix" {
+  description = "Prefix for bucket names (final name = prefix-purpose, must be globally unique)"
+  type        = string
+}
+
+variable "buckets" {
+  description = "Map of buckets to create: key = purpose suffix, value = enable versioning"
+  type        = map(bool)
+  default = {
+    "data"    = true
+    "logs"    = false
+    "backups" = true
+  }
+}
+
+variable "logs_archive_days" {
+  description = "Days before logs bucket objects transition to STANDARD_IA storage class"
+  type        = number
+  default     = 30
+}
+
+variable "logs_delete_days" {
+  description = "Days before logs bucket objects are deleted entirely"
+  type        = number
+  default     = 90
+}
+
+variable "reader_arns" {
+  description = "List of IAM principal ARNs granted read access to the data bucket"
+  type        = list(string)
+  default     = []
 }
