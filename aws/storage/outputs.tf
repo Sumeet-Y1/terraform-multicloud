56365be4abc2b97d1cb8ab56c8a6aa56be584954
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-04 21:20:48 +0530

    feat(aws-storage): update outputs for multiple bucket names and arns

--- aws/storage/outputs.tf
+++ aws/storage/outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_id" {
-  description = "ID (name) of the S3 bucket"
-  value       = aws_s3_bucket.main.id
+﻿output "bucket_names" {
+  description = "Names of all created buckets, keyed by purpose"
+  value       = { for k, b in aws_s3_bucket.main : k => b.id }
 }
 
-output "bucket_arn" {
-  description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.main.arn
+output "bucket_arns" {
+  description = "ARNs of all created buckets, keyed by purpose"
+  value       = { for k, b in aws_s3_bucket.main : k => b.arn }
 }
 
-output "bucket_regional_domain_name" {
-  description = "Regional domain name of the bucket (useful for referencing in other services)"
-  value       = aws_s3_bucket.main.bucket_regional_domain_name
+output "data_bucket_domain_name" {
+  description = "Regional domain name of the data bucket specifically"
+  value       = aws_s3_bucket.main["data"].bucket_regional_domain_name
 }
